dc14470e0f24100cc3b38d165cab721b22286e78
JDOXXED <149762125+JDOXXED@users.noreply.github.com>
2026-04-07 00:39:35 +0000

    Discussion1

---
+++ CodingJournal#1.py/Session1Prompt.py
@@ -0,0 +1,13 @@
+"""
+Created on Mon Apr  6 16:21:46 2026
+
+@author: jordan
+"""
+
+def main():
+    print("Jordan DeMaio")        
+    print("My favorite movie is Wall-e. It has been since I was 6 years old.")
+    print("My favorite food is steak. It has to be medium rare.")           
+    
+if __name__ == "__main__":
+    main()
---
+++ CodingJournal#1.py/Session2Prompt.py
@@ -0,0 +1,16 @@
+def main():
+    floatx = 12.4
+    floaty = 6.8
+    print(floatx + floaty)
+    print(type(floatx + floaty))
+    
+    integerx = 8
+    integery = 17
+    print(integerx - integery)
+    print(type(integerx - integery))
+
+    print(floatx * floaty)
+    print(type(floatx * floaty))
+    
+if __name__ == "__main__":
+    main()
